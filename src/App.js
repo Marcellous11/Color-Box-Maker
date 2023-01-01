ad4c7c936ed3a6784a963f20f7e773c9675b2861
Marcellous11 <1mcj001@gmail.com>
2022-12-31 23:10:16 -0600

    all done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList.js';
+
+function App() {
+	return (
+		<div className="App">
+			<div>
+				<TodoList />
+			</div>
+		</div>
+	);
+}
+
+export default App;
